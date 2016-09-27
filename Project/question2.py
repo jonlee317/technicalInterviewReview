45f3d41c5840cf57d14996a926d095af70f48b84
Jonathan Lee <jonlee317@users.noreply.github.com>
2016-09-27 00:52:25 -0700

    Create question2.py

---
+++ Project/question2.py
@@ -0,0 +1,11 @@
+# Question 2
+# Given a string a, find the longest palindromic substring contained in a. 
+# Your function definition should look like question2(a), and return a string.
+
+def question2(a):
+  pass
+  
+# test code
+
+question2("bloblobbor")
+question2("racecar")
