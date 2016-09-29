86a1ff8c40658323c13e4ea07d3ea83e0e1c2cbd
Jonathan Lee <jonlee317@users.noreply.github.com>
2016-09-28 17:38:34 -0700

    feat:  Implement question1 v1.0
    This is the initial implementation of question 1.  The code is not optimized and contains test statements.  However, it looks to be functional

--- Project/question1.py
+++ Project/question1.py
@@ -5,8 +5,48 @@
 # Your function definition should look like: question1(s, t) and return a boolean True or False.
 # 
 
-def question1(s,t):
-  pass
+def question1(s, t):
+	substringLength = len(t)
+	stringLength = len(s)
+	beginIndex = 0
+	endIndex = beginIndex + substringLength
+	matchedNum = []
+	checkList = [0 for i in range(substringLength)]
+	while endIndex <= stringLength:
+		testSubstring = list(s[beginIndex:endIndex])
+		mySubstring = list(t)
+		mySubstringLength = len(mySubstring)
+		print (testSubstring)
+		print (mySubstring)
 
-# Test code
-question("udacity", "ad")
+		# seach through testsubstring
+		while len(testSubstring) >0:
+			testChar = testSubstring.pop()
+			print ("here is test char")
+			print(testChar)
+			for i in range(mySubstringLength):
+				print (i)
+				print (mySubstring)
+				if mySubstring[i] == testChar and checkList[i] ==0:
+					matchedNum.append(1)
+					checkList[i] += 1
+					print (matchedNum)
+
+		if matchedNum.count(1) == substringLength:
+			break
+		else:
+			matchedNum = []
+			checkList = [0 for i in range(substringLength)]
+
+		beginIndex += 1
+		endIndex += 1
+		print (matchedNum)
+
+	if matchedNum.count(1) == substringLength:
+		return True
+	else:
+		return False
+  
+# testcode
+print(question1("udacity", "yit"))
+
