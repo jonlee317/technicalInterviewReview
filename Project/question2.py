87ee446a7d7f4fbae1ed2d61fe09e82c0348599c
Jonathan Lee <jon.lee.317@gmail.com>
2016-09-29 10:07:59 -0700

    feat: Implement dcbabcd pattern and return longest palindromic pattern

--- Project/question2.py
+++ Project/question2.py
@@ -1,46 +1,61 @@
 # Question 2
 # Given a string a, find the longest palindromic substring contained in a.
 # Your function definition should look like question2(a), and return a string.
-outputStringList = []
+
+# TODO: clean up the code and comment what each code does
+# TODO: see if there is any other more efficient method to implement
 
 def findMatch(inputString, begin, end, output):
+	# to take into account the letter corresponding to 'a' in the dcbabcd pattern
+	if end-begin == 2:
+		output += inputString[begin+1]
 	if begin >=0 and end < len(inputString):
 		if inputString[begin] == inputString[end]:
 			output = inputString[begin]+output+inputString[end]
 			output = findMatch(inputString, begin-1, end+1, output)
 		else:
 			output = output
-
 	return output
 
-
 def question2(a):
-	outputString = ""
+	outputStringList = []
+	outputString_type1 = ""
+	outputString_type2 = ""
 	initialIndex = 0
 	finalIndex = initialIndex + 1
 
 	while finalIndex < len(a):
-    # Only designed to find any double letters
-    # TODO:  need to find double letters and loop or recurse through the neighbors
-    # TODO:  need to find a single letter with double letter friends next to it
-		outputString = findMatch(a, initialIndex, finalIndex, outputString)
-		"""
-		if a[initialIndex] == a[finalIndex]:
-			outputString = a[initialIndex]+outputString+a[finalIndex]
-			print(outputString)
-			break
-		"""
+		# Implements the dcbaabcd pattern
+		outputString_type1 = findMatch(a, initialIndex, finalIndex, outputString_type1)
+		# Implements the dcbabcd pattern
+		outputString_type2 = findMatch(a, initialIndex, finalIndex+1, outputString_type2)
+
 		initialIndex += 1
 		finalIndex += 1
-		outputStringList.append(outputString)
-		outputString = ""
-	return outputString
 
-(question2('zeabbaezcc'))
-print outputStringList
+		# if there is nothing in the list simply append
+		if len(outputStringList) ==0:
+			outputStringList.append(outputString_type1)
+			outputStringList.append(outputString_type2)
+		# If there is something in the list place longer string in front else append
+		elif len(outputStringList) > 0:
+			if len(outputStringList[0]) > len(outputString_type1):
+				outputStringList.append(outputString_type1)
+			else:
+				outputStringList.insert(0,outputString_type1)
+			if len(outputStringList[0]) > len(outputString_type2):
+				outputStringList.append(outputString_type2)
+			else:
+				outputStringList.insert(0,outputString_type2)
+
+		outputString_type1 = ""
+		outputString_type2 = ""
+
+	return outputStringList[0]
 
 # test code
-
-
-question2("bloblobbor")
-question2("racecar")
+print(question2('zeabxbaezcc'))
+print(question2("bloblobbor"))
+print(question2("racecar"))
+print(question2("bloblobbbor"))
+print(question2("feffzracecarefz"))
