d6549f702269eb267e20974a2a81e2e83f9777a5
Jonathan Lee <jonlee317@users.noreply.github.com>
2016-09-28 19:17:09 -0700

    feat:  Implement search to find and print any double letters

--- Project/question2.py
+++ Project/question2.py
@@ -2,8 +2,31 @@
 # Given a string a, find the longest palindromic substring contained in a. 
 # Your function definition should look like question2(a), and return a string.
 
+
+def findMatch(begin, end, output):
+	if begin == end:
+		output = begin+output+end
+
+	return output
+
+
 def question2(a):
-  pass
+	outputString = ""
+	initialIndex = 0
+	finalIndex = initialIndex + 1
+
+	while finalIndex < len(a):
+    # Only designed to find any double letters
+    # TODO:  need to find double letters and loop or recurse through the neighbors
+    # TODO:  need to find a single letter with double letter friends next to it
+		if a[initialIndex] == a[finalIndex]:
+			outputString = a[initialIndex]+outputString+a[finalIndex]
+			print(outputString)
+			break
+		initialIndex += 1
+		finalIndex += 1
+
+question2('supperzz')
   
 # test code
 
