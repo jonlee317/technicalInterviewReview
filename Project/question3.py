a154666881c25bb39d8c1803b182a7ce8d5e284b
Jonathan Lee <jon.lee.317@gmail.com>
2016-10-01 09:30:29 -0700

    feat: Explore dictionary initially

--- Project/question3.py
+++ Project/question3.py
@@ -1,13 +1,23 @@
 # Question 3
-# Given an undirected graph G, find the minimum spanning tree within G. 
-# A minimum spanning tree connects all vertices in a graph with the smallest possible total weight of edges. 
+# Given an undirected graph G, find the minimum spanning tree within G.
+# A minimum spanning tree connects all vertices in a graph with the smallest possible total weight of edges.
 # Your function should take in and return an adjacency list structured like this:
 #
 # {'A': [('B', 2)],
-#  'B': [('A', 2), ('C', 5)], 
+#  'B': [('A', 2), ('C', 5)],
 #  'C': [('B', 5)]}
 # Vertices are represented as unique strings. The function definition should be question3(G)
 
 def question3(G):
-  pass
-  
+    nodeList = []
+    for item in G:
+        for val in G[item]:
+            print val
+
+myGraph = {'A': [('B',4), ('C',2)],
+            'B': [('A',4), ('D',3)],
+            'C': [('A',2), ('D',5)],
+            'D': [('B',3),('C',5)]
+}
+
+question3(myGraph)
