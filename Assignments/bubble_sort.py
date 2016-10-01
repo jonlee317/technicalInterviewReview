ba32ceaa68030c2c5e489d01f7e123d9a020407d
Jonathan Lee <jon.lee.317@gmail.com>
2016-10-01 09:34:55 -0700

    feat: Add assigments which includes sorts and stacks

---
+++ Assignments/bubble_sort.py
@@ -0,0 +1,17 @@
+input_array = [21,4,1,3,9,20,25,6,21,14]
+
+#no_flip = False
+count = 0
+while count <3:
+	#no_flip = True
+	count += 1
+	for i in range(len(input_array)):
+		if i+1 < len(input_array):
+			if input_array[i] > input_array[i+1]:
+				temp = input_array[i]
+				input_array[i] = input_array[i+1]
+				input_array[i+1] = temp
+				no_flip = False
+
+print input_array
+
